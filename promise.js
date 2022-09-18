9a2b70db39ae3c7ddbf3fcb60f4ff74804bd8333
liaoktora <113673764+liaoktora@users.noreply.github.com>
2022-09-18 19:52:18 +0700

    Update promise.js
    first

--- promise.js
+++ promise.js
@@ -1,8 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let tidakmarah = 0;
+let marah = 0;
+
+for (let i = 0; i < promiseTheaterIXX.length; i++) {
+    if (hasilsetelahmenonton.hasil = marah) {marah++}
+    else {tidakmarah++}
+}
+for (let i = 0; i < promiseTheaterVGC.length; i++) {
+    if (hasilsetelahmenonton.hasil = marah) {marah++}
+    else {tidakmarah++}
+}
+
+const promiseOutput = () => {
+    return new Promise((resolve, reject) => {
+        if (promiseTheaterIXX.length > 0 || promiseTheaterVGC.length >0) {
+            resolve("marah" = marah && "tidak marah" = tidakmarah);
+        } else {
+            reject("emosi = 0");
+        }
+    });
+};
 
 module.exports = {
   promiseOutput,
-};
+};lengt0
